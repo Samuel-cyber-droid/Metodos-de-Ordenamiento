e5234b20a380384f9096d9a7d606734e86fc41c4
Samuel-cyber-droid <78009369+Samuel-cyber-droid@users.noreply.github.com>
2023-11-10 20:48:06 -0600

    Create problema6-1.java

---
+++ problema6-1.java
@@ -0,0 +1,7 @@
+/*Un método de ordenación muy simple, pero no muy eficiente, de elementos x1, x2,
+x3,...xn en orden ascendente es el siguiente:
+Paso 1: Localizar el elemento más pequeño de la lista x1 a xn; intercambiarlo con x1.
+Paso 2: Localizar el elemento más pequeño de la lista x2 a xn, intercambiarlo con x2.
+Paso 3: Localizar el elemento más pequeño de la lista x3 a xn, intercambiarlo con xn.
+En el último paso, los dos últimos elementos se comparan e intercambian, si es neecesario, y la ordenación se termina.
+Escribir un programa para ordenar una lista de elementos, siguiendo este método.*/
