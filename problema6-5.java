1a0809eb519d4cc06ef801f738738d3212fd9211
Samuel-cyber-droid <78009369+Samuel-cyber-droid@users.noreply.github.com>
2023-11-10 20:52:45 -0600

    Create problema6-5.java

---
+++ problema6-5.java
@@ -0,0 +1,4 @@
+/*Realizar un programa que compare el tiempo de cálculo de las búsquedas secuencial y
+binaria. Una lista A se rellena con 2.000 enteros aleatorios en el rango 0 ... 1999 y a continuación se ordena.
+Una segunda lista B se rellena con 500 enteros aleatorios en el mismo rango.
+Los elementos de B se utilizan como claves de los algoritmos de búsqueda.*/
