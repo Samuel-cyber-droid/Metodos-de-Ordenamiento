370a07c6bdeb00a7d86a9b6e3076622396afadd1
Samuel-cyber-droid <78009369+Samuel-cyber-droid@users.noreply.github.com>
2023-11-10 20:45:32 -0600

    Create problema6-2.java

---
+++ problema6-2.java
@@ -0,0 +1,4 @@
+/*Dado un vector x de n elementos reales, donde n es impar, diseñar un método que
+calcule y devuelva la mediana de ese vector. La mediana es el valor tal que la mitad
+de los números son mayores que él y la otra mitad son menores. Escribir un programa
+de prueba.*/
