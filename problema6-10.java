9ecf44020b98f1eeefe57c64e9d4c7ea43820636
Samuel-cyber-droid <78009369+Samuel-cyber-droid@users.noreply.github.com>
2023-11-10 20:57:09 -0600

    Create problema6-10.java

---
+++ problema6-10.java
@@ -0,0 +1,5 @@
+/*Escribir un programa que genere un vector de 10.000 números aleatorios del 1 al 500.
+Realizar la ordenación del vector por dos métodos:
+•	 Binsort.
+•	 Radixsort.
+Escriba el tiempo empleado en la ordenación de cada método.*/
