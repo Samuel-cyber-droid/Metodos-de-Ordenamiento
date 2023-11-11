7f792e5e90f47554bd1c6b6d955ea24efafd9487
Samuel-cyber-droid <78009369+Samuel-cyber-droid@users.noreply.github.com>
2023-11-10 20:51:06 -0600

    Create problema6-4.java

---
+++ problema6-4.java
@@ -0,0 +1,3 @@
+/*Escribir un programa de consulta de teléfonos. Leer un conjunto de datos de 1.000
+nombres y números de teléfono de un archivo que contenga los números en orden aleatorio. 
+Las consultas han de poder realizarse por nombre y por número de teléfono*/
